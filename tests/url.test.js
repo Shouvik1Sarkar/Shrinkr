b2857ad225b348c114c0170632a8e63cb2201031
Shouvik1Sarkar <shouvik3501@gmail.com>
2026-04-20 00:48:27 +0530

    feat: add Redis integration with client configuration and update package dependencies

--- tests/url.test.js
+++ tests/url.test.js
@@ -27,130 +27,15 @@ afterAll(async () => {
   await mongoose.disconnect();
 }, 15000);
 
-describe("RANDOM TEST", () => {
-  it("Test random", async () => {
-    const res = await request(app).get("/api/v1/auth/test");
-    console.log("RES: ", res.text);
-    expect(res.status).toBe(200);
-    // expect(res.body.data).toHaveProperty("email", "test@example.com");
-    expect(res.text).toBe("Hello");
-  }, 15000);
-
-  it("should register a user", async () => {
-    const res = await request(app).post("/api/v1/auth/sign-up").send({
-      firstName: "test",
-      lastName: "lastTest",
-      userName: "test-1",
-      email: "test@example.com",
-      password: "Aa$123456",
-    });
-    // console.log("the data: ", res);
-    // console.log("STATUS: ", res.status);
-
-    expect(res.status).toBe(201);
-    expect(res.body.data).toHaveProperty("email", "test@example.com");
-  }, 15000);
-
-  it("should verify email", async () => {
-    // 1️⃣ Register
-    await request(app).post("/v1/api/auth/signUp").send(
-      {
-        firstName: "test",
-        lastName: "lastTest",
-        userName: "test-1",
-        email: "test@example.com",
-        password: "Aa$123456",
-      },
-      { new: true },
-    );
-
-    // 2️⃣ Find user and overwrite the OTP with a known one
-    const otp = 123456;
-    const encryptedOTP = crypto
-      .createHash("sha256")
-      .update(otp.toString())
-      .digest("hex");
-
-    const updated = await User.findOneAndUpdate(
-      { email: "test@example.com" },
-      {
-        emailVerificationToken: encryptedOTP,
-        emailVerificationTokenExpiry: Date.now() + 5 * 60 * 1000,
-      },
-    );
-    // console.log("User found:", updated?.email);
-    // console.log("Token in DB:", updated?.emailVerificationToken);
-    // console.log("Sending OTP:", otp);
-    // console.log("Hashed OTP:", encryptedOTP);
-    // console.log("User found and updated");
-    // 3️⃣ Verify with the known raw OTP
+describe("RANDOM TEST", async () => {
+  test("shorten url", async () => {
+    const cookies = await logInUser();
+    // console.log("USER LOGGEDIN");
     const res = await request(app)
-      .post("/api/v1/auth/verify") // adjust route
-      .send({ token: otp });
+      .get("/api/v1/user/logOut")
+      .set("Cookie", cookies);
 
-    //   console.log("Token in DBlll:", res);
-    // console.log("otp sendt");
-    // console.log("is email verified:", updated?.isEmailVerified);
-
-    expect(res.status).toBe(200);
-    expect(res.body.message).toBe("Email verified");
-  }, 15000);
-
-  it("should LogIn User", async () => {
-    // 1️⃣ Register
-    await request(app).post("/api/v1/auth/sign-up").send(
-      {
-        firstName: "test",
-        lastName: "lastTest",
-        userName: "test-1",
-        email: "test@example.com",
-        password: "Aa$123456",
-      },
-      { new: true },
-    );
-
-    // 2️⃣ Find user and overwrite the OTP with a known one
-    const otp = 123456;
-    const encryptedOTP = crypto
-      .createHash("sha256")
-      .update(otp.toString())
-      .digest("hex");
-
-    const updated = await User.findOneAndUpdate(
-      { email: "test@example.com" },
-      {
-        emailVerificationToken: encryptedOTP,
-        emailVerificationTokenExpiry: Date.now() + 5 * 60 * 1000,
-      },
-    );
-    // console.log("User found:", updated?.email);
-    // console.log("Token in DB:", updated?.emailVerificationToken);
-    // console.log("Sending OTP:", otp);
-    // console.log("Hashed OTP:", encryptedOTP);
-    // console.log("User found and updated");
-    // 3️⃣ Verify with the known raw OTP
-    const res = await request(app)
-      .post("/api/v1/auth/verification") // adjust route
-      .send({ token: otp });
-
-    //   console.log("Token in DBlll:", res);
-    // console.log("otp sendt");
-    // console.log("is email verified:", updated?.isEmailVerified);
-
-    // expect(res.status).toBe(200);
-    // expect(res.body.message).toBe("Email verified");
-
-    const loggedInUser = await request(app).post("/api/v1/auth/sign-in").send({
-      email: "test@example.com",
-      password: "Aa$123456",
-    });
-
-    console.log("LOGGEDIN USER", loggedInUser.body.data);
-
-    expect(loggedInUser.status).toBe(200);
-    expect(loggedInUser.body.message).toBe("User Logged In");
-
-    // data checks
-    expect(loggedInUser.body.data).toHaveProperty("email", "test@example.com");
+    expect(res.statusCode).toBe(200);
+    expect(res.body.message).toBe("Logged Out");
   }, 15000);
 });
